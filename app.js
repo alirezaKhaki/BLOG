088c6e9593c41e56716cb28c2fcbe8df4195f9aa
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-04-25 20:45:09 +0430

    bug fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true)
 mongoose.connect(
-    'mongodb://localhost:27017/blog', {
+    process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
