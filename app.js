13f53994f9183a41df5f4e4855896d76482fd94c
alirezaKhaki <77831895+alirezaKhaki@users.noreply.github.com>
2021-04-25 18:55:55 +0430

    mongo change for heroku

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ mongoose.connect(
     process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
+    }, () => {
+        console.log('connected to DB');
     }
 );;
 // view engine setup
